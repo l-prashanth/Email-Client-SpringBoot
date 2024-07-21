734d5335771c49b4dd2b3a4eaf33b46a56487032
L Prashanth <prashanth.py0101@gmail.com>
2024-07-21 18:47:52 +0530

    Merge branch 'main' of https://github.com/l-prashanth/Email-Client-SpringBoot into Second-Working
    # Conflicts:
    #	Dockerfile

--- src/main/java/com/prashanth/email/service/EmailSenderService.java
+++ src/main/java/com/prashanth/email/service/EmailSenderService.java
@@ -1,13 +1,6 @@
 package com.prashanth.email.service;
 
-import com.prashanth.email.model.InputFields;
-import jakarta.mail.MessagingException;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.ui.Model;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
